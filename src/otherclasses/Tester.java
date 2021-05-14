c0ed9f0d56e7ddc27750198fd78b669a53d169c2
Bálint Dániel <balint.daniel623@gmail.com>
2021-05-14 17:03:52 +0200

    Save
    Error message diplayed only on console

--- src/otherclasses/Tester.java
+++ src/otherclasses/Tester.java
@@ -14,71 +14,119 @@ public class Tester {
 	
 	// GLOBAL VARIABLES
 	
-	public boolean tesPayment(String[] data) {
-		if(isWSID(data[1]) && isCID(data[2]) && isType(data[3]) && isAmount(data[4]) && (isCardOrBankNumber(data[5]) || isCardOrBankNumber(data[6])) && isDate(data[7]))
-			return true;
-		else 
+	/**
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public boolean testCustomer(String[] data){
+		String msg = "";
+		msg += isWSID(data[0]);
+		msg += isCID(data[1]);
+		msg += isName(data[2]);
+		msg += isAddress(data[3]);
+		
+		if(msg.length() > 0) {			
+			System.out.println("A hibás rekord(ok): "+msg+" Ebben a sorban: "+arrayFormat(data));		
 			return false;
+		}else return true;
+	}
+	
+	/**
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public boolean testPayment(String[] data) {
+		String msg = "";
+		msg+= isWSID(data[1]);
+		msg+= isCID(data[2]);
+		msg+= isType(data[3]);
+		msg+= isAmount(data[4]);	
+		String bank = isCardOrBankNumber(data[5]);
+		String card = isCardOrBankNumber(data[6]);
+		if(card.length() > 0 || bank.length() > 0)
+			msg += bank.length() > 0 ? "BankAccountNumber: "+bank+" " : "CardNumber: "+card+" ";		
+		msg+= isDate(data[7]);
+		
+		if(msg.length() > 0) {			
+			System.out.println("A hibás rekord(ok): "+msg+" Ebben a sorban: "+arrayFormat(data));
+			return false;
+		}else return true;
 	}
 	
 	// TESTS
 	
-	private boolean isWSID(String wSID) {
+	private String isWSID(String wSID) {
 		pattern = Pattern.compile("^[WS]{2}\\d+$");
 		matcher = pattern.matcher(wSID);
-		if(matcher.matches())
-			return true;
-		else return false;
+		if(matcher.matches()) return "";
+		else return "WSID: "+wSID+" ";
 	}
 	
-	private boolean isCID(String cID) {
+	private String isCID(String cID) {
 		pattern = Pattern.compile("^[A]{1}\\d+$");
 		matcher = pattern.matcher(cID);
-		if(matcher.matches())
-			return true;
-		else return false;
-	}
-	
-	// PAYMENT TESTS
-	
-	private boolean isType(String type) {
-		if(type.equals("card") || type.equals("transfer"))
-			return true;
-		else return false;
-	}
-	
-	private boolean isAmount(String amount) {
-		pattern = Pattern.compile("^[1-9][0-9]*$");
-		matcher = pattern.matcher(amount);
-		if(matcher.matches())
-			return true;
-		else return false;
-	}
-	
-	private boolean isCardOrBankNumber(String cardNumber) {
-		pattern = Pattern.compile("^[1-9]+,[0-9]+E\\+15$");
-		matcher = pattern.matcher(cardNumber);		
-		if(matcher.matches())
-			return true;
-		else return false;
-	}
-	
-	private boolean isDate(String date) {
-		try {
-			Date d = new SimpleDateFormat("yyyy.MM.dd").parse(date);
-			return true;
-		}catch(Exception ex) {
-			return false;
-		}
+		if(matcher.matches()) return "";
+		else return "CID: "+cID+" ";
 	}
 	
 	// CUSTOMER TESTS
 	
-	private boolean isName(String name) {
-		return true;
+	private String isName(String name) {
+		pattern = Pattern.compile("^[AÁEÉIÍOÓÖÕUÚÜÛA-z][aáeéiíoóöõuúüûa-z]+ [AÁEÉIÍOÓÖÕUÚÜÛA-z][aáeéiíoóöõuúüûa-z]+$");
+		matcher = pattern.matcher(name);		
+		if(matcher.matches()) return "";
+		else return "Name: "+name+" ";
+	}
+		
+	private String isAddress(String address) {
+		pattern = Pattern.compile("^[A-z]+.?[A-z ]+[1-9][0-9]{3} .+ [0-9]+$");
+		matcher = pattern.matcher(address);		
+		if(matcher.matches()) return "";
+		else return "Address: "+address+" ";
 	}
 	
-	private boolean isAddress(String address) {
-		return true;
+	// PAYMENT TESTS
+	
+	private String isType(String type) {
+		if(type.equals("card") || type.equals("transfer")) return "";
+		else return "Type: "+type+" ";
+	}
+	
+	private String isAmount(String amount) {
+		pattern = Pattern.compile("^[1-9][0-9]*$");
+		matcher = pattern.matcher(amount);
+		if(matcher.matches()) return "";
+		else return "Amount: "+amount+" ";
+	}
+	
+	private String isCardOrBankNumber(String number) {
+		pattern = Pattern.compile("^[1-9]+,[0-9]+E\\+15$");
+		matcher = pattern.matcher(number);		
+		if(matcher.matches()) return "";
+		else return number;
+	}
+	
+	private String isDate(String date) {
+		try {
+			Date d = new SimpleDateFormat("yyyy.MM.dd").parse(date);
+			return "";
+		}catch(Exception ex) {
+			return "Date: "+date+" ";
+		}
 	}	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private String arrayFormat(String[] array) {
+		String re = "";
+		for(String s : array)
+			if(s.equals(array[0]))
+				re+=s;
+			else re += ", "+s;
+		return re;
+	}
 }
--- src/otpwebshop/MainClass.java
+++ src/otpwebshop/MainClass.java
@@ -9,30 +9,45 @@ public class MainClass{
 	// GLOBAL VARIABLES
 	
 	private Vector<Payment> payments = new Vector<Payment>();
+	private Vector<Customer> customers = new Vector<Customer>();
 	
 	// END OF GLOBAL VARIABLES
 	
+	/**
+	 * Constructor
+	 */
 	public MainClass() {
 		try {
-			readData();
-			System.out.println("done");
+			readData();			
 		}catch(Exception ex) {			
 			ex.printStackTrace();
 		}
 	}
 	
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	private void readData() throws Exception {		
 		Database databse = new Database();		
 		Tester tester = new Tester();
+		databse.connect("src/Assets/Database.db");
 		
-		databse.connect("src/Assets/Database.db");		
+		// Read customers				
+		for(String[] data : databse.select("SELECT * FROM customers"))
+			if(tester.testCustomer(data))
+				customers.add(new Customer(data));
+		
+		// Read payments		
 		for(String[] data : databse.select("SELECT * FROM payments"))
-			if(tester.tesPayment(data)) {
-				payments.add(new Payment(data));
-			}else
-				System.out.println("EZ bukta.");						
+			if(tester.testPayment(data))
+				payments.add(new Payment(data));					
 	}	
 	
+	/**
+	 * Main
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		new MainClass();
 	}
